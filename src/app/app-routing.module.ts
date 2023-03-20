db3d775730e457f24c0637903de0417b8cbc7129
GutierrezKenneth <kenneth.gutierrez.xentra@gmail.com>
2023-03-20 10:47:04 +0800

    Third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,18 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { pathToFileURL } from 'url';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children:[
+      {
+        path: 'home',
+        children:[
+         {
+          path:'',
+          loadChildren:() => import('../main/main.module').then(m =>m.MainPageModule)
+         }
+        ]
+      },
+      {
+        path: 'profile',
+        children:[
+         {
+          path:'',
+          loadChildren:() => import('../profile/profile.module').then(m =>m.ProfilePageModule)
+         }
+        ]
+      },
+      {
+        path: 'settings',
+        children:[
+         {
+          path:'',
+          loadChildren:() => import('../settings/settings.module').then(m =>m.SettingsPageModule)
+         }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
