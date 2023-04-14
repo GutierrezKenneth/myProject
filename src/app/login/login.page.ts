1f3b804921bea2a937d567c203ac191a08821c49
GutierrezKenneth <kenneth.gutierrez.xentra@gmail.com>
2023-04-14 13:07:46 +0800

    Seventh Commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,113 +1,159 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { Storage } from '@ionic/storage-angular';
-import { AlertController } from '@ionic/angular';
-import { SharedService } from '../shared.service';
-import { LoadingController } from '@ionic/angular';
+  import { Component } from '@angular/core';
+  import { Router } from '@angular/router';
+  import { Storage } from '@ionic/storage-angular';
+  import { AlertController } from '@ionic/angular';
+  import { SharedService } from '../shared.service';
+  import { LoadingController } from '@ionic/angular';
+  import { BiometryType, NativeBiometric } from "capacitor-native-biometric";
+import { tr } from 'date-fns/locale';
 
 
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
-})
-export class LoginPage  {
- public username:any;
- public password:any;
- public pname: any;
- public getPass: any;
-usernameTouched = false;
-passwordTouched = false;
-  constructor(private router: Router,private storage: Storage, private alertController: AlertController, private shared: SharedService,private loadingController: LoadingController) {this.storage.create(); }
+  @Component({
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
+  })
+  export class LoginPage  {
+  public username:any;
+  public password:any;
+  public pname: any;
+  public getPass: any;
+  usernameTouched = false;
+  passwordTouched = false;
+  public sessionTest:any
+    constructor(private router: Router,private storage: Storage, private alertController: AlertController, private shared: SharedService,private loadingController: LoadingController) {this.storage.create(); }
 
-  ngOnInit() {
+    ngOnInit() {
 
-    this.username = '';
-    this.password = '';
-  }
+      this.username = '';
+      this.password = '';
+    }
 
 
- 
+  
 
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      header: 'Alert',
-      subHeader: 'Important message',
-      message: 'Invalid password or Username!',
-      buttons: ['OK'],
+    async presentAlert() {
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        subHeader: 'Important message',
+        message: 'Invalid password or Username!',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+    }
+
+    onUsernameFocus() {
+      if(this.username == ''){
+        this.usernameTouched = true;
+      }
+
+      else{
+        this.usernameTouched = false;
+      }
+
+    }
+
+    onPasswordFocus() {
+      if(this.password == '')
+      {
+        this.passwordTouched = true;
+      }
+
+      else{
+        this.passwordTouched = false;
+      }
+        
+    }
+
+    
+    async logIn() {
+      if(this.username == ''){
+        this.onUsernameFocus();
+      } 
+      if(this.password== '')
+      {
+        this.onPasswordFocus();
+      }
+      
+      else{
+
+        const users = await this.storage['get']('users') || [];
+        const user = users.find((u: { username: any; password: any; }) => u.username === this.username && u.password === this.password);
+          if (user) {
+            this.pname = this.username;
+            this.getPass = this.password;
+            this.shared.setUsername(this.pname);
+            this.shared.setPassword(this.getPass);
+            this.sessionTest = sessionStorage.getItem("username");
+
+            if( this.sessionTest!= "") {
+              this.username ='';
+              this.password = '';
+              this.router.navigate(['/main']);
+            }
+            else{
+              sessionStorage.setItem("username",this.username)
+              sessionStorage.setItem("password", this.password)
+              this.username ='';
+              this.password = '';
+              this.router.navigate(['/main']);
+            }
+           
+          
+          
+
+          
+    
+        } else {
+            this.presentAlert();
+          }
+      }
+      
+    
+    }
+
+    signUp() {
+      this.router.navigate(['/signup']);
+    }
+
+    async biometrics(){
+      const result = await NativeBiometric.isAvailable();
+
+    if(!result.isAvailable) return;
+
+    const isFaceID = result.biometryType == BiometryType.FACE_ID;
+
+    const verified = await NativeBiometric.verifyIdentity({
+      reason: "For easy log in",
+      title: "Log in",
+      subtitle: "Maybe add subtitle here?",
+      description: "Maybe a description too?",
+    })
+      .then(() => true)
+      .catch(() => false);
+
+    if(!verified) return;
+
+    const credentials = await NativeBiometric.getCredentials({
+      server: "kennethgutierrez",
     });
 
-    await alert.present();
-  }
-
-  onUsernameFocus() {
-    if(this.username == ''){
-      this.usernameTouched = true;
-    }
-
-    else{
-      this.usernameTouched = false;
+    if(credentials.username){
+     this.username=credentials.username
+     this.password=credentials.password
+     this.logIn();
     }
 
   }
 
-  onPasswordFocus() {
-    if(this.password == '')
-    {
-      this.passwordTouched = true;
-    }
 
-    else{
-      this.passwordTouched = false;
-    }
-      
+
+
+
+
+
+
+
   }
-
-  
-  async logIn() {
-    if(this.username == ''){
-      this.onUsernameFocus();
-    }
-    if(this.password== '')
-    {
-      this.onPasswordFocus();
-    }
-    
-    else{
-
-      const users = await this.storage['get']('users') || [];
-      const user = users.find((u: { username: any; password: any; }) => u.username === this.username && u.password === this.password);
-        if (user) {
-          this.pname = this.username;
-          this.getPass = this.password;
-          this.shared.setUsername(this.pname);
-          this.shared.setPassword(this.getPass);
-          this.username ='';
-          this.password = '';
-          this.router.navigate(['/main']);
-        
-        
-
-         
-  
-       } else {
-          this.presentAlert();
-        }
-    }
-    
-   
-  }
-
-  signUp() {
-    this.router.navigate(['/signup']);
-  }
-
-
-
-
-
-
-
-
-}
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { NativeBiometric } from 'capacitor-native-biometric';
+import { tr } from 'date-fns/locale';
 
 @Component({
   selector: 'app-settings',
@@ -8,9 +10,27 @@ import { Router } from "@angular/router";
 })
 export class SettingsPage implements OnInit {
 
+
+  public hasBiometrics: boolean = false;
+  public isBiometricsEnabled: boolean = false;
   constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  async initiateBiometrics(){
+    const result = await NativeBiometric.isAvailable();
+    this.hasBiometrics = result.isAvailable;
+    // Save user's credentials
+    const credential = await NativeBiometric.getCredentials({
+      server: "kennethgutierrez",
+    }).then();
+    if(credential.username){
+      this.isBiometricsEnabled = true;
+    }
+    else{
+      this.isBiometricsEnabled = false;
+    }
   }
 
   logout(){
@@ -22,6 +42,22 @@ export class SettingsPage implements OnInit {
   }
   profile(){
     this.router.navigate(['/profile']);
+  }
+
+  enableBiometrics(){
+    // Save user's credentials
+    NativeBiometric.setCredentials({
+    username: sessionStorage.getItem("username")|| "",
+    password: sessionStorage.getItem("password")|| "",
+    server: "kennethgutierrez",
+}).then();
+  }
+
+  disableBiometrics(){
+    // Delete user's credentials
+    NativeBiometric.deleteCredentials({
+    server: "kennethgutierrez",
+}).then();
   }
 
 
